ba983082a786e2c9375f05073549b371aa7acafb
brodelp <brandon@bpenny.dev>
2023-06-05 16:29:36 -0500

    reserved space for header

--- test/script.js
+++ test/script.js
@@ -1,1 +1,25 @@
-console.log("Hello World!")
+const darkModeToggle = document.querySelector(".dark-mode-toggle");
+
+
+const enableDarkMode = () => {
+    document.body.classList.add("darkmode");
+
+    localStorage.setItem("darkMode", "enabled")
+};
+
+const disableDarkMode = () => {
+    document.body.classList.remove("darkmode");
+
+    localStorage.setItem("darkMode", "disabled")
+};
+
+
+
+darkModeToggle.addEventListener("click", () => {
+    let darkMode = localStorage.getItem("darkMode");
+    if (darkMode !== "enabled") {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+});
